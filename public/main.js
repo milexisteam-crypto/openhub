00685faf0047eee9bcb6a9fd7516f66f2ea49c28
Milexin <milexisteam@gmail.com>
2026-01-30 14:54:25 +0100

    v. 0.4.6

--- public/main.js
+++ public/main.js
@@ -1,3 +1,10 @@
 document.querySelector(".admin").addEventListener("click", () => {
     window.location.href = "login/admin"
 })
+document.querySelector(".browse").addEventListener("click", () => {
+    document.querySelector(".search").classList.remove("hide")
+})
+document.querySelector(".switch").addEventListener("click", () => {
+    document.querySelector(".switch").classList.toggle("turn")
+    document.querySelector("body").classList.toggle("dark")
+})
