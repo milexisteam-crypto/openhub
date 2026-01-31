65bc79d9f180549feed69e466477e61f18cfc001
Milexin <milexisteam@gmail.com>
2026-01-31 14:12:45 -0500

    t

--- public/main.js
+++ public/main.js
@@ -1,20 +1,12 @@
 document.querySelector(".admin").addEventListener("click", () => {
     window.location.href = "login/admin"
 })
-document.querySelector(".browse").addEventListener("click", () => {
-    document.querySelector(".search").classList.remove("hide")
-})
 document.querySelector(".switch").addEventListener("click", () => {
     document.querySelector(".switch").classList.toggle("turn")
     document.querySelector("body").classList.toggle("dark")
 })
 document.querySelector(".logo").addEventListener("click", () => {
     document.querySelector(".home").scrollIntoView({
-    behavior: "smooth"
-})
-})
-document.querySelector(".about").addEventListener("click", () => {
-    document.querySelector(".aboutsection").scrollIntoView({
     behavior: "smooth"
 })
 })
